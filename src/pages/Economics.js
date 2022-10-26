034b01b5d98ff32e943cfd190ec6c8e87678e1d6
SooBin An <jejupalette@naver.com>
2022-10-26 23:54:59 +0900

    feat : 기존 제작한 컴포넌트 활용(header, bottom button, phone background) ecomomics 기본 틀 제작

--- src/pages/Economics.js
+++ src/pages/Economics.js
@@ -1,5 +1,17 @@
+import BottomButton from '../components/BottomButton';
+import Title from '../components/Title';
+import Header from '../components/Header';
+import PhoneBackground from '../components/PhoneBackground';
+
 const Economics = () => {
-  return <div>Economics</div>;
+  return (
+    <div>
+      <Header fifthLineColor="#F8D459" />
+      <Title subTitle={'금융 혜택 알림'} mainTitle={'알뜰하게 챙겨드려요'} />
+      <PhoneBackground />
+      <BottomButton backLocation="/professionalism" nextLocation="/intuition" />
+    </div>
+  );
 };
 
 export default Economics;
