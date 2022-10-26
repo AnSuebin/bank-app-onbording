713639ca06331486dea079c5372c464e37d2d2c9
SooBin An <jejupalette@naver.com>
2022-10-26 23:54:51 +0900

    feat : 기존 제작한 컴포넌트 활용(header, bottom button, phone background) ecomomics 기본 틀 제작

--- src/pages/Convenience.js
+++ src/pages/Convenience.js
@@ -1,5 +1,17 @@
+import BottomButton from '../components/BottomButton';
+import Title from '../components/Title';
+import Header from '../components/Header';
+import PhoneBackground from '../components/PhoneBackground';
+
 const Convenience = () => {
-  return <div>Convenience</div>;
+  return (
+    <div>
+      <Header secondLineColor="#F8D459" />
+      <Title subTitle={'영상통화로 회원가입'} mainTitle={'편하게 시작하세요'} />
+      <PhoneBackground />
+      <BottomButton backLocation="/safety" nextLocation="/accessibility" />
+    </div>
+  );
 };
 
 export default Convenience;
