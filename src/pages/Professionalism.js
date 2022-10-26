c21ff27aac4d07dcceff6da0aaf59815ca1b5718
SooBin An <jejupalette@naver.com>
2022-10-26 23:55:33 +0900

    feat : 기존 제작한 컴포넌트 활용(header, bottom button, phone background) professionalism 기본 틀 제작

--- src/pages/Professionalism.js
+++ src/pages/Professionalism.js
@@ -1,5 +1,17 @@
+import BottomButton from '../components/BottomButton';
+import Title from '../components/Title';
+import Header from '../components/Header';
+import PhoneBackground from '../components/PhoneBackground';
+
 const Professionalism = () => {
-  return <div>Professionalism</div>;
+  return (
+    <div>
+      <Header fouthLindColor="#F8D459" />
+      <Title subTitle={'맞춤형 금융 비서'} mainTitle={'똑똑하게 관리하세요'} />
+      <PhoneBackground />
+      <BottomButton backLocation="/accessibility" nextLocation="/economics" />
+    </div>
+  );
 };
 
 export default Professionalism;
