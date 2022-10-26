99b4bc1e6e8c2cd4026c4ed5bb82a6baf63ee0ce
SooBin An <jejupalette@naver.com>
2022-10-26 23:55:52 +0900

    feat : 기존 제작한 컴포넌트 활용(header, bottom button, phone background) safety 기본 틀 제작

--- src/pages/Satety.js
+++ src/pages/Satety.js
@@ -1,10 +1,15 @@
-import CloseButton from '../components/CloseButton';
+import BottomButton from '../components/BottomButton';
+import Title from '../components/Title';
+import Header from '../components/Header';
+import PhoneBackground from '../components/PhoneBackground';
 
 const Safety = () => {
   return (
     <div>
-      Safety
-      <CloseButton />
+      <Header firstLineColor="#F8D459" />
+      <Title subTitle={'금융 사기 감지'} mainTitle={'안심하고 거래하세요'} />
+      <PhoneBackground />
+      <BottomButton backLocation="/" nextLocation="/convenience" />
     </div>
   );
 };
