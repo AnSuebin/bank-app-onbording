17ffd50c60b050406b17b3b742bdfc364b00dbb4
SooBin An <jejupalette@naver.com>
2022-10-26 19:26:53 +0900

    feat : 개발 편의를 위한 홈의 페이지별 버튼 제작

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
-  return <div>home</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Home</h1>
+      <button onClick={() => navigate('/safety')}>safety</button>
+      <button onClick={() => navigate('/convenience')}>convenience</button>
+      <button onClick={() => navigate('/accessibility')}>accessibility</button>
+      <button onClick={() => navigate('/professionalism')}>
+        professionalism
+      </button>
+      <button onClick={() => navigate('/Economics')}>Economics</button>
+      <button onClick={() => navigate('/intuition')}>intuition</button>
+    </div>
+  );
 };
 
 export default Home;
