c08671c5541ef64fbd269f2c0fcbb72e90cdd795
SooBin An <jejupalette@naver.com>
2022-10-26 23:54:35 +0900

    feat : 기존 제작한 컴포넌트 활용(header, bottom button, phone background) Accessibility 기본 틀 제작

--- src/pages/Accessibility.js
+++ src/pages/Accessibility.js
@@ -1,5 +1,20 @@
+import BottomButton from '../components/BottomButton';
+import Title from '../components/Title';
+import Header from '../components/Header';
+import PhoneBackground from '../components/PhoneBackground';
+
 const Accessibility = () => {
-  return <div>Accessibility</div>;
+  return (
+    <div>
+      <Header thirdLineColor="#F8D459" />
+      <Title subTitle={'말하는 상담원'} mainTitle={'필요할 때 찾아주세요'} />
+      <PhoneBackground />
+      <BottomButton
+        backLocation="/convenience"
+        nextLocation="/professionalism"
+      />
+    </div>
+  );
 };
 
 export default Accessibility;
