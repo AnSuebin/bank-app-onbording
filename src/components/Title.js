253e6804a621b7e56d8e238312fd70399dff37ae
SooBin An <jejupalette@naver.com>
2022-10-29 22:11:57 +0900

    design: 페이드인 효과 추가

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,16 +1,38 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+to {
+  opacity: 1;
+  transform: translateY(0);
+}
+`;
 
 const SubTitle = styled.div`
+  font-family: 'Spoqa Han Sans Neo';
   color: #1355b4;
   font-size: 18px;
   font-weight: 400;
+  animation-duration: 0.75s;
+  animation-timing-function: ease-in-out;
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
 `;
 
 const MainTitle = styled.div`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 700;
   margin-top: 14px;
   color: #222222;
   font-size: 26px;
-  font-weight: 700;
+  animation-duration: 0.75s;
+  animation-timing-function: ease-in-out;
+  animation-name: ${fadeIn};
+  animation-fill-mode: forwards;
 `;
 
 const TitleCotainer = styled.div`
