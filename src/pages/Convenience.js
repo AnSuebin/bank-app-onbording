133c599a8c3d08d4c0e20f9e0ed92b0268688d38
SooBin An <jejupalette@naver.com>
2022-10-29 22:28:10 +0900

    fix : 동영상 업로드 문제 수정

--- src/pages/Convenience.js
+++ src/pages/Convenience.js
@@ -4,14 +4,6 @@ import Header from '../components/Header';
 import PhoneBackground from '../components/PhoneBackground';
 import { mockData } from '../data/mockData';
 import { useEffect, useState } from 'react';
-import mainVideo from '../utils/convenience/main-video.mov';
-import subVideo from '../utils/convenience/sub-video.mov';
-import styled from 'styled-components';
-
-const MainVideo = styled.video`
-  width: 294px;
-  height: 290px;
-`;
 
 const Convenience = () => {
   const [title, setTitle] = useState('');
@@ -27,7 +19,7 @@ const Convenience = () => {
       <Header firstLineColor="#F8D459" secondLineColor="#F8D459" />
       <Title subTitle={title} mainTitle={subtitle} />
       <PhoneBackground />
-      <MainVideo src={mainVideo} loop autoPlay muted />
+
       <BottomButton backLocation="/safety" nextLocation="/accessibility" />
     </div>
   );
