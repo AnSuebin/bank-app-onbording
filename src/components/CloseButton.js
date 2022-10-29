2c2b317cf36a97b176b6605e8dcd19d2d06a9769
SooBin An <jejupalette@naver.com>
2022-10-29 22:10:32 +0900

    design: 삭제버튼 디자인 수정

--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -4,21 +4,28 @@ const Close = styled.button`
   background-color: transparent;
   border: none;
   font-size: 16px;
-  padding: 10px;
+  /* padding: 10px; */
   text-decoration: none;
   color: #222222;
   cursor: pointer;
+  padding: 10px;
+  font-family: 'SpoqaHanSans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  color: #6e767e;
 `;
 
 const CloseContainer = styled.div`
   display: flex;
+  margin-top: 8px;
   justify-content: end;
 `;
 
 const CloseButton = () => {
   return (
     <CloseContainer>
-      <Close>╳</Close>
+      <Close>건너뛰기</Close>
     </CloseContainer>
   );
 };
