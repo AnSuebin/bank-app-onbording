7b5b66869a1c6f450dba5111a9e42ffa4b478ab0
SooBin An <jejupalette@naver.com>
2022-11-04 23:37:39 +0900

    feat : typing 기능 글귀 제작

---
+++ src/components/TypingText.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Typing = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-weight: 700;
+  font-size: 21px;
+  line-height: 26px;
+  letter-spacing: -0.02em;
+`;
+
+const TypingText = () => {
+  const txt = '노후연금 추천해줘';
+  const [isTypingTime, setIsTypingTime] = useState(false);
+  const [Text, setText] = useState('');
+  const [Count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsTypingTime(true);
+    }, 1700);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [isTypingTime]);
+
+  useEffect(() => {
+    if (isTypingTime) {
+      const interval = setInterval(() => {
+        setText(Text + txt[Count]);
+        setCount(Count + 1);
+      }, 130);
+      if (Count === txt.length) {
+        clearInterval(interval);
+      }
+      return () => clearInterval(interval);
+    }
+  });
+
+  return <>{isTypingTime === true && <Typing>{Text}</Typing>}</>;
+};
+
+export default TypingText;
