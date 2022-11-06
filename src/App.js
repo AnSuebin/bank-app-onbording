62b084e817fd811602276967ec0c1c0bf57c4de1
SooBin An <jejupalette@naver.com>
2022-11-07 01:14:33 +0900

    fix : 폰트가 모바일에만 적용 안되는 이슈 해결

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 import Safety from './pages/page1-safety/Safety';
 import Convenience from './pages/page2-convenience/Convenience';
